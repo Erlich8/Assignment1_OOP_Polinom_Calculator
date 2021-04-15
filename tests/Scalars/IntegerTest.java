109af65a66c0095aba609355623a293827733e36
Erlich <d7erlichman@gmail.com>
2021-04-16 01:17:46 +0300

    making tests.

---
+++ tests/Scalars/IntegerTest.java
@@ -0,0 +1,39 @@
+package Scalars;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IntegerTest {
+
+    @BeforeEach
+    void setUp() {
+        Scalar num1 = new Integer(1);
+        num-1
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void addition() {
+    }
+
+    @Test
+    void multiplication() {
+    }
+
+    @Test
+    void power() {
+    }
+
+    @Test
+    void neg() {
+    }
+
+    @Test
+    void sign() {
+    }
+}
